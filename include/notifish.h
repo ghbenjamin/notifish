5ad5f019507cda91bb3e41ca4b60fe621f0808bc
Benjamin Hammond <brh1316@gmail.com>
2020-10-12 20:24:59 +0100

    Working remote notifications via zmq sockets.

--- include/notifish.h
+++ include/notifish.h
@@ -3,6 +3,9 @@
 #include <string_view>
 #include <filesystem>
 #include <unordered_map>
+#include <optional>
+
+#include <fmt/format.h>
 
 #ifdef WIN32
 #define NF_WINDOWS
@@ -18,32 +21,63 @@ namespace nf
 
 struct NotifyInfo
 {
-    std::string command;
-    std::string title;
-    std::string message;
-
     bool is_silent = false;
     bool is_local = true;
-    int notify_min_timeout_ms = -1;
-
     std::string remote_server;
+    std::string host_name;
+    std::string command;
 
-    int return_code = -1;
+    int exit_code = -1;
+
+
+    std::string to_json() const;
+    static std::optional<NotifyInfo> from_json( std::string_view json_str );
 };
 
 
 
 // Core
-void do_notify( NotifyInfo& ninfo );
+void do_command(NotifyInfo& ninfo );
 void notify_local( NotifyInfo& ninfo );
-int start_daemon(int port);
+void notify_remote( NotifyInfo& ninfo );
+
+[[noreturn]] int start_daemon( int port );
 
 // Config
-
-static constexpr const char* CONFIG_FILE_NAME = "notifish.conf";
-
 std::filesystem::path get_config_path();
 void ensure_config_file_exists();
 std::unordered_map<std::string, std::string> get_config_data();
+
+
+// Util
+
+std::string get_local_hostname();
+
+// Logging
+
+template <typename T, typename... Args>
+void log_to_console( const char* verb, T const& message, Args&&...args )
+{
+    std::cout << verb << fmt::format( message, std::forward<Args>(args)... ) << std::endl;
+}
+
+template <typename T, typename... Args>
+void log_warn( T const& message, Args&&...args )
+{
+    log_to_console( "Warning: ", message, std::forward<Args>(args)... );
+}
+
+template <typename T, typename... Args>
+void log_error( T const& message, Args&&...args )
+{
+    log_to_console( "Error: ", message, std::forward<Args>(args)... );
+}
+
+template <typename T, typename... Args>
+void log_info( T const& message, Args&&...args )
+{
+    log_to_console( "", message, std::forward<Args>(args)... );
+}
+
 
 }
--- src/notifish.cpp
+++ src/notifish.cpp
@@ -7,6 +7,9 @@
 #include <algorithm>
 
 #include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+#include <zmq.hpp>
 
 #ifdef NF_WINDOWS
 #define NOMINMAX
@@ -16,21 +19,101 @@
 #undef GetObject
 #endif
 
+
+namespace nf
+{
 static constexpr int MAX_TITLE_SIZE = 63;
 static constexpr int MAX_MESSAGE_SIZE = 254;
+static constexpr const char* CONFIG_FILE_NAME = "notifish.conf";
+static int DEFAULT_PORT = 1316;
+
+}
+
+std::string nf::NotifyInfo::to_json() const
+{
+    // Construct a rapidjson object to contain our NotifyInfo data
+    rapidjson::Document doc;
+    auto& alloc = doc.GetAllocator();
+
+    doc.SetObject();
+
+    auto sr_command = rapidjson::StringRef( this->command.c_str() );
+    doc.AddMember( "command", sr_command, alloc );
+
+    doc.AddMember( "is_silent", this->is_silent, alloc );
+    doc.AddMember( "is_local", this->is_local, alloc );
+
+    auto sr_remote_server = rapidjson::StringRef( this->remote_server.c_str() );
+    doc.AddMember( "remote_server", sr_remote_server, alloc );
+
+    auto sr_hostname = rapidjson::StringRef( this->host_name.c_str() );
+    doc.AddMember( "host_name", sr_hostname, alloc );
+
+    doc.AddMember( "exit_code", this->exit_code, alloc );
+
+
+    rapidjson::StringBuffer sb;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(sb);
+    doc.Accept(writer);
+
+    return sb.GetString();
+}
+
+std::optional<nf::NotifyInfo> nf::NotifyInfo::from_json( std::string_view json_str )
+{
+    nf::NotifyInfo ninfo;
+    rapidjson::Document doc;
+    doc.Parse( json_str.data(), json_str.size() );
+
+    if ( doc.HasParseError() )
+    {
+        return {};
+    }
+
+    ninfo.command = doc.FindMember("command")->value.GetString();
+    ninfo.is_silent = doc.FindMember("is_silent")->value.GetBool();
+    ninfo.is_local = doc.FindMember("is_local")->value.GetBool();
+    ninfo.remote_server = doc.FindMember("remote_server")->value.GetString();
+    ninfo.host_name = doc.FindMember("host_name")->value.GetString();
+    ninfo.exit_code = doc.FindMember("exit_code")->value.GetInt();
+
+    return ninfo;
+}
 
 void nf::notify_local(NotifyInfo& ninfo)
 {
+    std::string title;
+    std::string message;
+
+    if ( ninfo.exit_code == 0 )
+    {
+        title = "Command completed";
+    }
+    else
+    {
+        title = "Command failed";
+    }
+
+
+    if ( ninfo.is_local )
+    {
+        message = ninfo.command;
+    }
+    else
+    {
+        message = fmt::format( "{} - {}", ninfo.host_name, ninfo.command );
+    }
+
 #ifdef NF_WINDOWS
 
-    int title_len = std::min( MAX_TITLE_SIZE, (int)ninfo.title.size() );
-    int msg_len = std::min( MAX_MESSAGE_SIZE, (int)ninfo.message.size() );
+    int title_len = std::min( nf::MAX_TITLE_SIZE, (int)title.size() );
+    int msg_len = std::min( nf::MAX_MESSAGE_SIZE, (int)message.size() );
 
     NOTIFYICONDATA data = { sizeof(data) };
 
     data.uFlags = NIF_GUID | NIF_INFO;
 
-    if (ninfo.return_code == 0)
+    if (ninfo.exit_code == 0)
     {
         data.dwInfoFlags = NIIF_INFO;
     }
@@ -44,8 +127,8 @@ void nf::notify_local(NotifyInfo& ninfo)
         data.dwInfoFlags = data.dwInfoFlags | NIIF_NOSOUND;
     }
 
-    strncpy_s( data.szInfoTitle, ninfo.title.data(), title_len );
-    strncpy_s( data.szInfo, ninfo.message.data(), msg_len );
+    strncpy_s( data.szInfoTitle, title.data(), title_len );
+    strncpy_s( data.szInfo, message.data(), msg_len );
 
     BOOL result = Shell_NotifyIcon(NIM_ADD, &data);
     if (!result)
@@ -58,8 +141,59 @@ void nf::notify_local(NotifyInfo& ninfo)
 #endif
 }
 
-int nf::start_daemon( int port )
+void nf::notify_remote(nf::NotifyInfo &ninfo)
 {
+    assert( !ninfo.is_local );
+
+    auto json = ninfo.to_json();
+
+    // Connect to the remote socket and push the json string
+    std::string zmq_binding = fmt::format( "tcp://{}", ninfo.remote_server );
+    zmq::context_t zmq_context{1};
+    zmq::socket_t socket{zmq_context, zmq::socket_type::push};
+
+    socket.connect( zmq_binding );
+    socket.send( zmq::buffer( json ), zmq::send_flags::none );
+
+    socket.close();
+    zmq_context.close();
+}
+
+[[noreturn]] int nf::start_daemon( int port )
+{
+    std::string zmq_binding = fmt::format( "tcp://*:{}", port );
+
+    zmq::context_t zmq_context{1};
+    zmq::socket_t socket{zmq_context, zmq::socket_type::pull};
+
+    socket.bind( zmq_binding );
+    log_info( "nf daemon started on port {}", port );
+
+    for (;;)
+    {
+        zmq::message_t request;
+        socket.recv (request);
+
+        auto data = request.to_string_view();
+
+        auto ninfo = nf::NotifyInfo::from_json( data );
+
+        if ( !ninfo )
+        {
+            log_error( "Malformed server request:" );
+            log_info("{}\n", data);
+            continue;
+        }
+        else
+        {
+            log_info( "{}\n", data );
+            nf::notify_local( ninfo.value() );
+        }
+    }
+
+    socket.close();
+    zmq_context.close();
+
     return 0;
 }
 
@@ -93,7 +227,7 @@ void nf::ensure_config_file_exists()
     }
 
     std::ofstream ofs(path);
-    ofs << "{}\n";
+    ofs << "{\n  \"config\": {\n  }\n}\n";
     ofs.close();
 
     return;
@@ -114,38 +248,50 @@ std::unordered_map<std::string, std::string> nf::get_config_data()
 
     if (doc.HasParseError())
     {
-
+        log_warn("could not read configuration file");
+        return data;
     }
     else if (!doc.HasMember("config"))
     {
-
+        log_warn("config file is readable but malformed");
+        return data;
     }
 
     return std::move(data);
 }
 
-void nf::do_notify(nf::NotifyInfo &ninfo)
+void nf::do_command(nf::NotifyInfo &ninfo)
 {
+    ninfo.exit_code = system( ninfo.command.c_str() );
+
     if ( ninfo.is_local )
     {
-        ninfo.return_code = system( ninfo.command.c_str() );
-
-        if (ninfo.return_code == 0)
-        {
-            ninfo.title = "Program completed";
-        }
-        else
-        {
-            ninfo.title = "Program failed";
-        }
-
-        ninfo.message = ninfo.command;
         nf::notify_local( ninfo );
     }
     else
     {
-
+        notify_remote( ninfo );
     }
+}
+
+std::string nf::get_local_hostname()
+{
+#ifdef NF_WINDOWS
+    TCHAR buf[MAX_COMPUTERNAME_LENGTH + 1];
+    DWORD buf_len;
+
+    auto ret = GetComputerNameEx( (COMPUTER_NAME_FORMAT)0, buf, &buf_len );
+
+    if (ret)
+    {
+        return std::string(buf);
+    }
+    else
+    {
+        return "<unknown machine>";
+    }
+#else
+#endif
 }
 
 std::unordered_map<std::string, std::string> parse_command_line(int argc, char** argv)
@@ -216,23 +362,44 @@ int main( int argc, char** argv )
     config_args.merge( cmd_args );
     auto args = std::move(config_args);
 
-    if ( args.contains("help")
-            || args.contains("h")
-            || args.empty()
-            || !args.contains("target_command")
-            || args["target_command"].empty() )
+    if ( args.contains("help") || args.contains("h") )
     {
         std::cout << "Help text here" << std::endl;
         return 0;
     }
     else if ( args.contains("daemon") )
     {
+        if ( args.contains("target_command") && !args["target_command"].empty() )
+        {
+            nf::log_error( "Positional arguments and daemon are mutually exclusive" );
+            return 1;
+        }
 
+        int port = nf::DEFAULT_PORT;
+        if ( args.contains("port") )
+        {
+            try
+            {
+                port = std::stoi( args["port"] );
+            }
+            catch ( std::exception ex )
+            {
+                nf::log_error( "Port needs to be a number (got '{}')", args["port"] );
+                return 1;
+            }
+        }
+
+        nf::start_daemon(port);
     }
     else
     {
-        nf::NotifyInfo ninfo;
+        if ( !args.contains("target_command") || args["target_command"].empty() )
+        {
+            nf::log_error( "No command specified" );
+            return 1;
+        }
 
+        nf::NotifyInfo ninfo;
         ninfo.command = args["target_command"];
 
         if ( args.contains("server") )
@@ -245,17 +412,18 @@ int main( int argc, char** argv )
             ninfo.is_local = true;
         }
 
-        if (args.contains("min-timeout"))
-        {
-            ninfo.notify_min_timeout_ms = std::stoi(args["min-timeout"]);
-        }
-
         if (args.contains("silent"))
         {
             ninfo.is_silent = true;
         }
 
+        if (args.contains("port"))
+        {
+            nf::log_warn( "Port argument only applies to daemon mode - use the server argument for notify mode" );
+        }
 
-        nf::do_notify( ninfo );
+        ninfo.host_name = nf::get_local_hostname();
+
+        nf::do_command(ninfo);
     }
 }
